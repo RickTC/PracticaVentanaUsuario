55c1b9bf438061845a29234090c90a2d840de1d1
Ricardo Tapia <RickTC@localhost.localdomain>
2013-10-25 10:18:29 -0500

    Evento del boton aceptar listo, if con comparacion de password y usuario

--- PracticaVentanaUsuario/PracticaVentanaUsuario/MainWindow.cs
+++ PracticaVentanaUsuario/PracticaVentanaUsuario/MainWindow.cs
@@ -13,4 +13,37 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
+
+	protected void OnSalirClicked (object sender, System.EventArgs e)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	protected void OnAceptaClicked (object sender, System.EventArgs e)
+	{
+		if (password.Text=="123456"&& usuario.Text=="ricardo")
+		{
+			MessageDialog hola = new MessageDialog(
+				null,
+				DialogFlags.Modal,
+				MessageType.Info,
+				ButtonsType.None,
+				"Hola Bienvenido"
+			);
+			hola.Show();
+		}
+		
+		else
+		{
+			MessageDialog error = new MessageDialog(
+			
+				null,
+				DialogFlags.Modal,
+				MessageType.Info,
+				ButtonsType.None,
+				"Error verifique password y/o Usuario"
+			);
+			error.Show();
+		}
+	}
 }
